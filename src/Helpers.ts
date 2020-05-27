7f9c95761e7b7044c7d29334fbba720ebaf020a5
Danny Rottstegge <danny@rottstegge.net>
2020-05-27 13:23:43 +0200

    Add randomNumber and modify unique

--- src/Helpers.ts
+++ src/Helpers.ts
@@ -7,9 +7,22 @@ export const sleep = (ms: number): Promise<void> => {
     });
 };
 
-export const createDateTimeString = (date?: Date) => {
+export const createUnique = (date?: Date) => {
     const dt = date ? date : new Date();
-    return `${dt.getFullYear()}${dt.getMonth()}${dt.getDate()}${dt.getHours()}${dt.getMinutes()}${dt.getSeconds()}${dt.getMilliseconds()}`;
+    return `${dt
+        .getFullYear()
+        .toString()
+        .substr(
+            -2
+        )}${dt.getMonth()}${dt.getDate()}${dt.getHours()}${dt.getMinutes()}${dt.getSeconds()}-${generateRandomNumber(
+        { min: 1, max: 9 }
+    )}`;
+};
+
+export const generateRandomNumber = (opt?: { min?: number; max?: number }) => {
+    const min = opt?.min ? opt?.min : 0;
+    const max = opt?.max ? opt?.max : 0;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 export const findFiles = (rootDirectory: string, pattern?: RegExp) => {
