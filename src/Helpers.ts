ac3ab6accb951532f6a10079491a34574a6e1149
Danny Rottstegge <danny@rottstegge.net>
2020-05-31 10:36:50 +0200

    Fix unique

--- src/Helpers.ts
+++ src/Helpers.ts
@@ -9,15 +9,23 @@ export const sleep = (ms: number): Promise<void> => {
 
 export const createUnique = (date?: Date) => {
     const dt = date ? date : new Date();
-    return `${dt
-        .getFullYear()
-        .toString()
-        .substr(
-            -2
-        )}${dt.getMonth()}${dt.getDate()}${dt.getHours()}${dt.getMinutes()}${dt.getSeconds()}${dt
-        .getMilliseconds()
-        .toString()
-        .padStart(3, '0')}${generateRandomNumber({ min: 1, max: 9 })}`;
+    const year = dt.getFullYear().toString().substr(-2);
+    const month = padStartNumber(dt.getMonth(), 2, '0');
+    const day = padStartNumber(dt.getDate(), 2, '0');
+    const hours = padStartNumber(dt.getHours(), 2, '0');
+    const minutes = padStartNumber(dt.getMinutes(), 2, '0');
+    const seconds = padStartNumber(dt.getSeconds(), 2, '0');
+    const milliseconds = padStartNumber(dt.getMilliseconds(), 3, '0');
+    const random = generateRandomNumber({ min: 1, max: 9 });
+    return `${year}${month}${day}${hours}${minutes}${seconds}${milliseconds}${random}`;
+};
+
+export const padStartNumber = (
+    value: number,
+    length: number,
+    char?: string
+): string => {
+    return value.toString().padStart(length, char);
 };
 
 export const generateRandomNumber = (opt?: { min?: number; max?: number }) => {
