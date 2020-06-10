abe27bc6ddf6ac74b5c2f8f49846ea5961f5b220
Danny Rottstegge <danny@rottstegge.net>
2020-06-11 01:00:54 +0200

    Build

--- lib/helpers/FileHelper.d.ts
+++ lib/helpers/FileHelper.d.ts
@@ -1,2 +1,3 @@
 export declare const findFiles: (rootDirectory: string, pattern?: RegExp | undefined) => string[];
 export declare const mergeFiles: (files: string[], outfile: string) => void;
+export declare const deleteFolderRecursive: (path: string) => void;
