687b27fddcfb0f36902b6c4572564221b50d141e
Danny Rottstegge <danny@rottstegge.net>
2020-06-12 22:00:26 +0200

    Stop recorder in sempahore if used manager

--- src/services/RecorderManager.ts
+++ src/services/RecorderManager.ts
@@ -35,14 +35,18 @@ export class RecorderManager {
 
     constructor(options?: RecorderManagerOptions) {
         this._options = { ...defaultRecorderManagerOptions, ...options };
-        if (
-            this._options.maxConcurrentlyCreatingOutfiles &&
-            this._options.maxConcurrentlyCreatingOutfiles > 0
-        ) {
+        if (this.isUseSemaphore) {
             this._semaphore = new Semaphore(
                 this._options.maxConcurrentlyCreatingOutfiles
             );
         }
+    }
+
+    public get isUseSemaphore(): boolean {
+        return (
+            this._options.maxConcurrentlyCreatingOutfiles !== undefined &&
+            this._options.maxConcurrentlyCreatingOutfiles > 0
+        );
     }
 
     public get options(): RecorderManagerOptions {
@@ -55,6 +59,13 @@ export class RecorderManager {
     ): IRecorderItem {
         const recorderOptions: RecorderOptions = this
             ._options as RecorderOptions;
+        const autocreateOutputInSemaphore =
+            this.isUseSemaphore &&
+            this._options.automaticallyCreateOutfileIfExitedAbnormally;
+
+        if (autocreateOutputInSemaphore) {
+            recorderOptions.automaticallyCreateOutfileIfExitedAbnormally = false;
+        }
 
         recorderOptions.onStateChange = (
             newState: RecorderState,
@@ -73,8 +84,15 @@ export class RecorderManager {
                         );
                     }
                     if (
-                        this._options.autoRemoveWhenFinished &&
-                        newState == RecorderState.FINISH
+                        newState == RecorderState.EXITED_ABNORMALLY &&
+                        autocreateOutputInSemaphore
+                    ) {
+                        this.stop(
+                            this.recorders[sessionInfo.recorderId]!.request
+                        );
+                    } else if (
+                        newState == RecorderState.FINISH &&
+                        this._options.autoRemoveWhenFinished
                     ) {
                         this.remove(sessionInfo.recorderId);
                     }
