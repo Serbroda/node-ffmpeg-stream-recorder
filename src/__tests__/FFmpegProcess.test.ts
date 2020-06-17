bf8b308b5297e73869be0fd4f6495fb7a676e329
Danny Rottstegge <danny@rottstegge.net>
2020-06-17 15:50:47 +0200

    Update test

--- src/__tests__/FFmpegProcess.test.ts
+++ src/__tests__/FFmpegProcess.test.ts
@@ -1,14 +1,17 @@
-import { FFmpegProcess } from '../index';
+import { FFmpegProcess, FFmpegProcessResult } from '../index';
 import { join } from 'path';
 import * as fs from 'fs';
 import { sleep } from '../helpers/ThreadingHelper';
+import { FFmpegProcessOptions } from '../services/FFmpegProcess';
 
-jest.setTimeout(120000);
+jest.setTimeout(20 * 1000);
 
+// ffmpeg -y -i https://test-streams.mux.dev/pts_shift/master.m3u8 -c:v copy -c:a copy -f segment -segment_list out.ffcat seg_%03d.ts
 const testUrl = 'https://test-streams.mux.dev/pts_shift/master.m3u8';
 const testingDirectory = __dirname + '/out';
 
 const cleanTestDirectory = () => {
+    sleep(2000);
     fs.readdirSync(testingDirectory).forEach((f) => {
         fs.unlinkSync(join(testingDirectory, f));
     });
@@ -23,37 +26,80 @@ beforeAll(() => {
 beforeEach(() => cleanTestDirectory());
 afterAll(() => cleanTestDirectory());
 
-it('should download segments', async (p) => {
-    const onExitCallback = (
-        code: number,
-        planned?: boolean,
-        signal?: NodeJS.Signals
-    ) => {
-        expect(fs.readdirSync(testingDirectory).length).toBeGreaterThan(0);
-        p();
-    };
-    const process = new FFmpegProcess();
-    process.start(
-        [
-            '-y',
-            '-i',
-            testUrl,
-            '-c:v',
-            'copy',
-            '-c:a',
-            'copy',
-            '-f',
-            'segment',
-            '-segment_list',
-            'out.ffcat',
-            'seg_%03d.ts',
-        ],
-        {
-            workDirectory: testingDirectory,
-            onExit: onExitCallback,
+const args = [
+    '-y',
+    '-i',
+    testUrl,
+    '-c:v',
+    'copy',
+    '-c:a',
+    'copy',
+    '-f',
+    'segment',
+    '-segment_list',
+    'out.ffcat',
+    'seg_%03d.ts',
+];
+const options: FFmpegProcessOptions = {
+    workDirectory: testingDirectory,
+    printMessages: false,
+};
+
+it('should exit normally and download segment files', (done: jest.DoneCallback) => {
+    const callback = (result: FFmpegProcessResult) => {
+        try {
+            expect(result.exitCode).toBe(0);
+            expect(
+                fs
+                    .readdirSync(testingDirectory)
+                    .filter((f) => f.endsWith('.ts')).length
+            ).toBeGreaterThan(0);
+            done();
+        } catch (error) {
+            done(error);
         }
-    );
-    await sleep(5000);
+    };
+
+    new FFmpegProcess().start(args, {
+        ...options,
+        onExit: callback,
+    });
+});
+
+it('should kill planned', (done: jest.DoneCallback) => {
+    const callback = (result: FFmpegProcessResult) => {
+        try {
+            expect(result.plannedKill).toBe(true);
+            done();
+        } catch (error) {
+            done(error);
+        }
+    };
+
+    const process = new FFmpegProcess();
+    process.start(args, {
+        ...options,
+        onExit: callback,
+    });
+    sleep(2000);
     process.kill();
-    await sleep(200);
+});
+
+it('should wait for killed', (done: jest.DoneCallback) => {
+    const process = new FFmpegProcess();
+    const callback = (result: FFmpegProcessResult) => {
+        try {
+            expect(process.waitForProcessKilled()).toBe(true);
+            done();
+        } catch (error) {
+            done(error);
+        }
+    };
+
+    process.start(args, {
+        ...options,
+        onExit: callback,
+    });
+    sleep(2000);
+    process.kill();
 });
