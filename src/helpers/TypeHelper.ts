b791924a210434aeb375aac1cad880b5d5847173
Danny Rottstegge <danny@rottstegge.net>
2020-07-28 02:30:53 +0200

    Add interface to convert to json

--- src/helpers/TypeHelper.ts
+++ src/helpers/TypeHelper.ts
@@ -4,3 +4,11 @@ export const ofType = <T>(obj: any, instance: any): obj is T => {
     }
     return obj instanceof instance;
 };
+
+export interface FromJson<T, R extends T> {
+    fromJson(obj: T, ...args: any): R;
+}
+
+export interface ToJson<T> {
+    toJson(...args: any): T;
+}
