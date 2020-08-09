312391ee05e85578d31e366176f5d7ae2176d68b
Danny Rottstegge <danny@rottstegge.net>
2020-08-09 03:05:49 +0200

    Fix: Use correct var

--- lib/helpers/FileHelper.d.ts
+++ lib/helpers/FileHelper.d.ts
@@ -2,7 +2,7 @@ import * as path from 'path';
 export declare const findFiles: (rootDirectory: string, pattern?: string | RegExp | undefined) => string[];
 export declare const filenameMatchesPattern: (filename: string, pattern: string | RegExp) => boolean;
 export declare const mergeFiles: (files: string[], outfile: string) => void;
-export declare const deleteFolderRecursive: (path: string, filesOnly?: boolean | undefined) => void;
+export declare const deleteFolderRecursive: (p: string, filesOnly?: boolean | undefined) => void;
 export declare const tryDeleteFileTimes: (path: string, retries?: number, times?: number) => void;
 export declare const tryDeleteFile: (path: string) => boolean;
 export declare const mkdir: (...directories: string[]) => void;
