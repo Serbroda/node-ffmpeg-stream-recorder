725bdde63289be5f33d205b4efc0dfac840d8f29
Danny Rottstegge <danny@rottstegge.net>
2020-08-16 18:05:00 +0200

    Fix fetcher config

--- lib/config.d.ts
+++ lib/config.d.ts
@@ -1,6 +1,6 @@
 export interface FFmpegStreamRecorderConfiguration {
     executable: string;
-    fetcher: (url: RequestInfo | any, init?: RequestInit | any) => Promise<Response | any>;
+    fetcher?: (url: RequestInfo | any, init?: RequestInit | any) => Promise<Response | any>;
 }
 export declare let configuration: FFmpegStreamRecorderConfiguration;
 export declare function configure(config: Partial<FFmpegStreamRecorderConfiguration>): void;
