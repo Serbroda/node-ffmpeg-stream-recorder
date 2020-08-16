6d8619243aee4202810970c95c41ba382c2050f4
Danny Rottstegge <danny@rottstegge.net>
2020-08-16 16:03:02 +0200

    Add function to map variants indexed

--- lib/services/HLSParser.d.ts
+++ lib/services/HLSParser.d.ts
@@ -2,12 +2,13 @@ import { types } from 'hls-parser';
 import MediaPlaylist = types.MediaPlaylist;
 import MasterPlaylist = types.MasterPlaylist;
 import Variant = types.Variant;
-import { ArrayIndexed } from '../models/IndexedVariant';
+import { ArrayIndexed } from '../models/ArrayIndexed';
 import { VariantResolutionOption } from '../models';
 export declare class HLSParser {
     static parseManifest(manifest: string): MasterPlaylist | MediaPlaylist;
     static parseUrl(url: string, fetcher?: (url: RequestInfo | any, init?: RequestInit | any) => Promise<Response | any>): Promise<MasterPlaylist | MediaPlaylist>;
     static stringify(hls: MasterPlaylist | MediaPlaylist): string;
+    static mapIndexedVariants(variants: Variant[]): ArrayIndexed<Variant>[];
     static findVariant(master: MasterPlaylist, search: VariantResolutionOption): ArrayIndexed<Variant> | undefined;
     static findVariant(master: MasterPlaylist, predicate: (this: void, value: Variant, index: number, obj: readonly Variant[]) => boolean): ArrayIndexed<Variant> | undefined;
     filterVariants(master: MasterPlaylist, predicate: (this: void, value: Variant, index: number, obj: readonly Variant[]) => boolean): ArrayIndexed<Variant>[];
