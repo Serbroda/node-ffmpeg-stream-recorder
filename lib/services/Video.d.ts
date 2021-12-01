4868ec94b8b62dc312e47c3527005f7771e8148d
Danny Rottstegge <danny@rottstegge.net>
2021-12-01 18:15:21 +0100

    Add metadata method

--- lib/services/Video.d.ts
+++ lib/services/Video.d.ts
@@ -3,8 +3,18 @@ export interface ThumbnailOptions {
     outfile: string;
     resolution: string;
     offsetSeconds: number;
+    override: boolean;
+}
+export interface VideoMetadata {
+    name: string;
+    path: string;
+    duration: number;
+    size: number;
+    created: Date;
+    type: string;
 }
 export declare class Video {
+    getMetadata(file: string, options?: Partial<FFmprobeOptions>): Promise<VideoMetadata>;
     getDuration(file: string, options?: Partial<FFmprobeOptions>): Promise<number>;
     createThumbnail(filename: string, options?: Partial<ThumbnailOptions>): Promise<string>;
 }
--- src/services/Video.ts
+++ src/services/Video.ts
@@ -1,14 +1,40 @@
 import { FFmprobeOptions, FFprobeProcess } from './FFprobeProcess';
 import * as path from 'path';
+import * as fs from 'fs';
 import { FFmpegProcess } from './FFmpegProcess';
 
 export interface ThumbnailOptions {
     outfile: string;
     resolution: string;
     offsetSeconds: number;
+    override: boolean;
+}
+
+export interface VideoMetadata {
+    name: string;
+    path: string;
+    duration: number;
+    size: number;
+    created: Date;
+    type: string;
 }
 
 export class Video {
+    async getMetadata(file: string, options?: Partial<FFmprobeOptions>): Promise<VideoMetadata> {
+        const fileStats = fs.statSync(file);
+        const ext = path.extname(file);
+        const filename = path.basename(file, ext);
+        const duration = await this.getDuration(file, options);
+        return {
+            name: filename,
+            path: file,
+            duration,
+            size: fileStats.size,
+            created: fileStats.mtime,
+            type: ext,
+        };
+    }
+
     async getDuration(file: string, options?: Partial<FFmprobeOptions>): Promise<number> {
         const result = await new FFprobeProcess().exec(
             ['-i', file, '-show_entries', 'format=duration', '-v', 'quiet', '-of', 'csv=p=0'],
@@ -26,9 +52,18 @@ export class Video {
                     outfile: path.join(path.dirname(filename), `${name}.jpg`),
                     resolution: '640x360',
                     offsetSeconds: 2,
+                    override: false,
                 },
                 ...options,
             };
+
+            if (fs.existsSync(opt.outfile)) {
+                if (opt.override) {
+                    fs.rmSync(opt.outfile);
+                } else {
+                    throw new Error(`Thumbnail already exists`);
+                }
+            }
 
             const prc = new FFmpegProcess();
             prc.onExit.once((result) => {
